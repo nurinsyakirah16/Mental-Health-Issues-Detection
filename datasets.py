7a00feafe0461b543784c004b3f1e82fb0bdae3f
nurinsyakirah16 <101792320+nurinsyakirah16@users.noreply.github.com>
2024-01-09 20:49:02 +0800

    datasets.py

--- datasets.py
+++ datasets.py
@@ -1,5 +1,5 @@
 import pandas as pd
 
 original_dataset = pd.read_csv("finaldataset(real).csv")
-cleaned_dataset = pd.read_csv("/preprocessed_datasetDSP.csv")
+cleaned_dataset = pd.read_csv("preprocessed_datasetDSP.csv")
 dataset_used = pd.read_csv("dataset_used.csv")
